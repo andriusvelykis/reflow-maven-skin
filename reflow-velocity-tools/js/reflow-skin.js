8e8719afa809a7dba8010997ffea38d52dd93641
Andrius Velykis <andrius@velykis.lt>
2014-06-22 09:24:34 -0700

    Creating site for reflow-velocity-tools, 1.1.1

--- reflow-velocity-tools/js/reflow-skin.js
+++ reflow-velocity-tools/js/reflow-skin.js
@@ -6,39 +6,12 @@
 	$(function(){
 
 		var $window = $(window)
-	
+
 		// Start carousel
 		$(function() {
 			$('.carousel').carousel();
 		});
-		
-		// Support for smooth scrolling and back button while scrolling
-		// Note: only run if smoothScroll is enabled
-		if (typeof($.smoothScroll) == typeof(Function)) {
-			$('a[href^="#"]:not([href^="#carousel"])').live('click', function() {
-				var slashedHash = '#/' + this.hash.slice(1);
-				if ( this.hash ) {
 
-					if ( slashedHash === location.hash ) {
-						$.smoothScroll({scrollTarget: this.hash});
-					} else {
-						$.bbq.pushState(slashedHash);
-					}
-
-					return false;
-				}
-			});
-
-			$(window).bind('hashchange', function(event) {
-				var tgt = location.hash.replace(/^#\/?/,'');
-				if ( document.getElementById(tgt) ) {
-					$.smoothScroll({scrollTarget: '#' + tgt});
-				}
-			});
-
-			$(window).trigger('hashchange');
-		}
-		
 		// activate syntax higlighting with highlight.js
 		// Note: only run if `hljs` exists
 		if (typeof hljs != 'undefined')
